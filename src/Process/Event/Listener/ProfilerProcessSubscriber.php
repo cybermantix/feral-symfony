2ac19f65b529ab652a53460bf0995c9f68928aff
gclift <gclift@gmail.com>
2024-11-07 14:44:33 -0700

    adding dev only ProfilerProcessSubscriber.

--- src/Process/Event/Listener/ProfilerProcessSubscriber.php
+++ src/Process/Event/Listener/ProfilerProcessSubscriber.php
@@ -2,15 +2,15 @@
 
 namespace Feral\Symfony\Process\Event\Listener;
 
-use Feral\Core\Process\Event\ProcessExceptionEvent;
-use Feral\Core\Process\Event\ProcessNodeNotifyEvent;
 use Feral\Symfony\Process\DataCollector\Trace\ProcessTraceCollectorInterface;
 use Feral\Core\Process\Event\ProcessEndEvent;
 use Feral\Core\Process\Event\ProcessNodeAfterEvent;
 use Feral\Core\Process\Event\ProcessNodeBeforeEvent;
 use Feral\Core\Process\Event\ProcessStartEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\DependencyInjection\Attribute\When;
 
+#[When(env: 'dev')]
 class ProfilerProcessSubscriber implements EventSubscriberInterface
 {
     public function __construct(private ProcessTraceCollectorInterface $collector){}
